aba483dfd4f7d471d9409a030d206c936797450b
Mauri Mustonen <mauri_mustonen@hotmail.com>
2020-02-03 19:18:16 +0200

    i3 config

--- install.sh
+++ install.sh
@@ -6,3 +6,7 @@ ln -s ~/dotfiles/vim/ftplugin ~/.vim
 # Kitty
 mkdir -p ~/.config/kitty
 ln -s ~/dotfiles/kitty/kitty.conf ~/.config/kitty/kitty.conf
+
+# i3
+mkdir -p ~/.i3
+ln -s ~/dotfiles/i3/config ~/.i3/config
