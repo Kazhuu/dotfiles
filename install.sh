90153cdffbf0fbe6bef1051535d6d047776ad1dc
Mauri Mustonen <mauri_mustonen@hotmail.com>
2020-02-04 10:01:14 +0200

    install fix

--- install.sh
+++ install.sh
@@ -1,12 +1,7 @@
 # Vim
 mkdir -p ~/.vim
 ln -s ~/dotfiles/vim/.vimrc ~/.vimrc
-# Check to prevent making symbolic links to this repository.
-if [ -f /.vim/ftplugin ]; then
-    ln -s ~/dotfiles/vim/ftplugin ~/.vim/ftplugin
-else
-    printf "~/.vim/ftplugin file exists\n"
-fi
+ln -s ~/dotfiles/vim/ftplugin ~/.vim
 
 # Kitty
 mkdir -p ~/.config/kitty
