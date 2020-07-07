6557ec66976cd24f6b785c9f99155aef90045ce3
Mauri Mustonen <mauri_mustonen@hotmail.com>
2020-07-07 10:19:13 +0300

    spotify live song skip

---
+++ spotify/skip-live.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# From https://github.com/Peyphour/spotify-skip-live/blob/master/spotify-skip
+while [ 1 ]; do
+    contains=`playerctl metadata xesam:title | grep -i live`
+    if [ -n "$contains" ]; then
+        playerctl next
+    fi
+    sleep 1
+done
