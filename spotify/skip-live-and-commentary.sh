86c830c463638c7b4b21f18fa69e0412a3d73c4b
Mauri Mustonen <mauri_mustonen@hotmail.com>
2020-07-24 16:06:37 +0300

    skip commentary songs on spotify

--- spotify/skip-live-and-commentary.sh
+++ spotify/skip-live-and-commentary.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # From https://github.com/Peyphour/spotify-skip-live/blob/master/spotify-skip
 while [ 1 ]; do
-    contains=`playerctl metadata xesam:title | grep -i live`
+    contains=`playerctl metadata xesam:title | grep -i "live\|commentary"`
     if [ -n "$contains" ]; then
         playerctl next
     fi
